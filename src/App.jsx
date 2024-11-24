ec5260b9805f40a069e27fd310598d4089a57cf0
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-24 18:23:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/Hero"
+
+const App = () => {
+  return (
+    <main className="relative max-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  )
+}
+
+export default App
